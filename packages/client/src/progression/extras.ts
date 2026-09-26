9a25afebcc65ff9fc6eb491ef3f2f7a7ebaa7e1a
Quin Carter <quin.carter@gmail.com>
2026-09-25 23:30:57 -0400

    music: The Hood villain-loses track

--- packages/client/src/progression/extras.ts
+++ packages/client/src/progression/extras.ts
@@ -357,6 +357,7 @@ export const TRACK_TITLES: Readonly<Record<string, string>> = {
   "scenarios/loki/villain-loses.mp3": "The Trickster Unmasked",
   "scenarios/loki/villain-wins.mp3": "Asgard Kneels",
   "scenarios/the-hood/battle.mp3": "The Syndicate Rises",
+  "scenarios/the-hood/villain-loses.mp3": "The Syndicate Scatters",
   "scenarios/the-hood/villain-wins.mp3": "A Cloak Over the City",
   "campaigns/trors/finale.mp3": "Anthem of the Returning Sun",
   "campaigns/gmw/finale.mp3": "The Longest Watch Ends",
